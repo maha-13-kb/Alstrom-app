0ff54d8ea259b1b8d9cdcd3958d813a73e7c672e
maha-13-kb <mahashruthispips@gmail.com>
2024-07-12 10:01:28 +0530

    first commit

---
+++ src/test/java/com/training/oracle/bms/AlstromAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.oracle.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlstromAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
